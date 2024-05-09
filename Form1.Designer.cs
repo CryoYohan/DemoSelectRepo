abbf4467e395028d6a2ca11051cb81c250dbd02b
itsmecyril <cyrilypil@gmail.com>
2024-05-10 00:29:20 +0800

    sad

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,31 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(85, 126);
+            textBox1.Location = new Point(114, 150);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(292, 150);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(176, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 75);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -44,6 +61,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkSlateGray;
             ClientSize = new Size(517, 445);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
             Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +74,7 @@
         #endregion
 
         private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
