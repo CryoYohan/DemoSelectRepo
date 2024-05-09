0a1622b7801b721e6567841c900fbf37814813ee
itsmecyril <cyrilypil@gmail.com>
2024-05-10 00:40:20 +0800

    ADDED FEATURES

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,10 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // textBox1
@@ -55,12 +59,56 @@
             button1.TabIndex = 2;
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(114, 115);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 14);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(294, 115);
+            button3.Name = "button3";
+            button3.Size = new Size(98, 14);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(154, 152);
+            button4.Name = "button4";
+            button4.Size = new Size(22, 21);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(328, 150);
+            button5.Name = "button5";
+            button5.Size = new Size(22, 21);
+            button5.TabIndex = 6;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkSlateGray;
             ClientSize = new Size(517, 445);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -76,5 +124,9 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
